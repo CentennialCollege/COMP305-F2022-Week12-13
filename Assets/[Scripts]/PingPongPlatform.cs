1525947e26c14b9626e9660d56dfd286fb7989fd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-05 17:49:25 -0400

    updated moving platforms

--- Assets/[Scripts]/PingPongPlatform.cs
+++ Assets/[Scripts]/PingPongPlatform.cs
@@ -12,11 +12,25 @@ public enum PlatformDirection
     DIAGONAL_DOWN
 }
 
+[System.Serializable]
+public struct Arrows
+{
+    public GameObject right;
+    public GameObject left;
+    public GameObject up;
+    public GameObject down;
+    public GameObject diagonalUp;
+    public GameObject diagonalDown;
+}
+
 
 public class PingPongPlatform : MonoBehaviour
 {
     public Vector2 startPoint;
     public PlatformDirection direction;
+
+    public Arrows arrows;
+    
 
     [Range(1.0f, 20.0f)]
     public float horizontalDistance = 8.0f;
@@ -40,15 +54,21 @@ public class PingPongPlatform : MonoBehaviour
         {
             case PlatformDirection.HORIZONTAL:
                 transform.position = new Vector3(Mathf.PingPong(Time.time * horizontalSpeed, horizontalDistance) + startPoint.x, startPoint.y, 0.0f);
+                arrows.right.SetActive(true);
+                arrows.left.SetActive(true);
                 break;
             case PlatformDirection.VERTICAL:
                 transform.position = new Vector3(startPoint.x,Mathf.PingPong(Time.time * verticalSpeed, verticalDistance) + startPoint.y, 0.0f);
+                arrows.up.SetActive(true);
+                arrows.down.SetActive(true);
                 break;
             case PlatformDirection.DIAGONAL_UP:
                 transform.position = new Vector3(Mathf.PingPong(Time.time * horizontalSpeed, horizontalDistance) + startPoint.x, Mathf.PingPong(Time.time * verticalSpeed, verticalDistance) + startPoint.y, 0.0f);
+                arrows.diagonalUp.SetActive(true);
                 break;
             case PlatformDirection.DIAGONAL_DOWN:
                 transform.position = new Vector3(Mathf.PingPong(Time.time * horizontalSpeed, horizontalDistance) + startPoint.x, startPoint.y - Mathf.PingPong(Time.time * verticalSpeed, verticalDistance), 0.0f);
+                arrows.diagonalDown.SetActive(true);
                 break;
         }
 
